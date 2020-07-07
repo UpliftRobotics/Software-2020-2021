58e2e8bc01575e8a34d828c61106c43a35ea7039
matthewself12 <matthewself12@users.noreply.github.com>
2020-07-07 13:59:38 -0400

    sdfvdfb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CurvePoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CurvePoint.java
@@ -7,5 +7,30 @@ public class CurvePoint {
     public double turnSpeed;
     public double followDistance;
     public double pointLength;
-    public double slowDown
+    public double slowDownTurnRad;
+    public double slowDownTurnAmount;
+
+    public CurvePoint(double x, double y, double moveSpeed, double turnSpeed, double followDistance, double pointLength, double slowDownTurnRad, double slowDownTurnAmount) {
+
+        this.x = x;
+        this.y = y;
+        this.moveSpeed = moveSpeed;
+        this.turnSpeed = turnSpeed;
+        this.followDistance = followDistance;
+        this.pointLength = pointLength;
+        this.slowDownTurnRad = slowDownTurnRad;
+        this.slowDownTurnAmount = slowDownTurnAmount;
+    }
+
+    public CurvePoint(CurvePoint thisPoint) {
+        x = thisPoint.x;
+        y = thisPoint.y;
+        moveSpeed = thisPoint.moveSpeed;
+        turnSpeed = thisPoint.turnSpeed;
+        followDistance = thisPoint.followDistance;
+        pointLength = thisPoint.pointLength;
+        slowDownTurnRad = thisPoint.slowDownTurnRad;
+        slowDownTurnAmount = thisPoint.slowDownTurnAmount;
+    }
+
 }
