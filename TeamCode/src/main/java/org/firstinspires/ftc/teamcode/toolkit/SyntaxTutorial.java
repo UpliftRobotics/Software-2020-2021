396d58d27300ddf760944b628add2318dd2760ef
matthewself12 <matthewself12@users.noreply.github.com>
2020-07-23 16:50:25 -0400

    Syntax

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/toolkit/SyntaxTutorial.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.toolkit;
+
+
+public class SyntaxTutorial {
+
+    public static int classNum;
+
+    public static void main(String[] args) {
+
+        classNum = 25;
+
+    }
+}
