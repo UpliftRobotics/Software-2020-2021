d9fde3ffaaf22fae28fba4cc9d1ee4b8db5de0d7
matthewself12 <matthewself12@users.noreply.github.com>
2020-08-06 15:21:29 -0400

    Update Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,41 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "TeleOp", group = "OpModes")
 public class Teleop extends OpMode {
+
+    // Declare robot and variables.
     Robot robot = new Robot();
     double leftY;
     double rightX;
-    double rightY;
     double leftX;
-    double fl;
-    double fr;
-    double bl;
-    double br;
+    double leftFrontPower;
+    double rightFrontPower;
+    double leftBackPower;
+    double rightBackPower;
 
-        @Override
-        public void init() {
-        }
+    @Override
+    public void init() {
+    }
 
-        @Override
-        public void loop() {
-            leftY = -gamepad1.left_stick_y;
-            rightX = gamepad1. right_stick_x;
-            leftX = gamepad1.left_stick_x;
+    @Override
+    public void loop() {
+        leftY = -gamepad1.left_stick_y;
+        rightX = gamepad1. right_stick_x;
+        leftX = gamepad1.left_stick_x;
 
-            fl = leftY + leftX + rightX;
-            fr = leftY - leftX - rightX;
-            bl = leftY - leftX + rightX;
-            br = leftY + leftX - rightX;
+        leftFrontPower = leftY + leftX + rightX;
+        rightFrontPower = leftY - leftX - rightX;
+        leftBackPower = leftY - leftX + rightX;
+        rightBackPower = leftY + leftX - rightX;
 
-            robot.leftFront.setPower(Range.clip(fl,-1,1));
-            robot.rightFront.setPower(Range.clip(fr,-1,1));
-            robot.leftBack.setPower(Range.clip(bl,-1,1));
-            robot.rightBack.setPower(Range.clip(br,-1,1));
+        robot.leftFront.setPower(Range.clip(leftFrontPower,-1,1));
+        robot.rightFront.setPower(Range.clip(rightFrontPower,-1,1));
+        robot.leftBack.setPower(Range.clip(leftBackPower,-1,1));
+        robot.rightBack.setPower(Range.clip(rightBackPower,-1,1));
 
-        }
+    }
 }
