4135cd842f35f09cc281c62b9633fb013f590e0e
Dravani <42421220+Dravani@users.noreply.github.com>
2020-09-09 14:47:57 -0400

    Update Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -4,14 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.toolkit.ULLinearOpMode;
-import org.firstinspires.ftc.teamcode.toolkit.ULOpMode;
 
 @TeleOp(name = "TeleOp", group = "OpModes")
 public class Teleop extends ULLinearOpMode {
 
     // Declare robot and variables.
     Robot robot;
-    double rightX;
+    double rightXDirection;
     double leftY;
     double leftX;
 
@@ -30,7 +29,7 @@ public class Teleop extends ULLinearOpMode {
 
             // initialize the gamepad stick values to the three needed axes
             leftY = Range.clip(-gamepad1.left_stick_y, -1, 1);
-            rightX = Range.clip(gamepad1.right_stick_x, -1, 1);
+            rightXDirection = Range.clip(gamepad1.right_stick_x, -1, 1);
             leftX = Range.clip(gamepad1.left_stick_x, -1, 1);
 
             // Note: The following algorithm was inspired by the webpage https://seamonsters-2605.github.io/archive/mecanum/. It explains this concept very well.
@@ -41,8 +40,8 @@ public class Teleop extends ULLinearOpMode {
             // find the magnitude, or hypotenuse of the left joystick and scale it down by dividing by the max it could be
             double magnitude = Math.sqrt(Math.pow(leftX, 2) + Math.pow(leftY, 2));
 
-            // find the turnValue directly from the rightX input value (halved for smoothness)
-            double turnValue = 0.75 * rightX;
+            // find the turnValue directly from the rightXDirection input value (halved for smoothness)
+            double turnValue = 0.75 * rightXDirection;
 
             // set the powers using the 2 specific equations and clip the result
             robot.slideDirection(magnitude, joystickAngle, turnValue);
